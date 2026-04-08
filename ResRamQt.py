345001bc54519303b14d4113d7723e9a789f521c
Likun <kentclk@outlook.com>
2026-04-08 19:18:30 -0400

    Fix directory path initialization in SpectrumApp class

--- ResRamQt.py
+++ ResRamQt.py
@@ -218,7 +218,7 @@ class SpectrumApp(QMainWindow):
     """
     def __init__(self):
         super().__init__()
-        self.dir = ""
+        self.dir = "./example/"
         # multithread
         self.threadpool = QThreadPool()
         # print("Multithreading with maximum %d threads" %self.threadpool.maxThreadCount())
@@ -809,7 +809,7 @@ class SpectrumApp(QMainWindow):
     def initialize(self):
         """Initialize the GUI
         """
-        self.dir = "./example"
+        self.dir = "./example/"
         self.load_files()
         
         # Reset plot item storage
